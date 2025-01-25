7eb116e131bb2b7e06f7b0f6c7c9897c88801305
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2025-01-25 18:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+// function firstWord(s) {
+//   // your code here
+// }
+
+// // Do not change the code below
+
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
+
 function firstWord(s) {
-  // your code here
+  // Trim any leading or trailing spaces
+  s = s.trim();
+
+  // Find the index of the first space
+  const firstSpaceIndex = s.indexOf(' ');
+
+  // If there is no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring up to the first space
+  return s.slice(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
